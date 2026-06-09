a40d299163660bbee6821dc43fe4e827d715f23a
Yuri Teixeira <yuriteixeirac@proton.me>
2026-06-09 18:43:22 -0300

    fix: modelagem mais acurada

--- apps/agenda/models/agendamento.py
+++ apps/agenda/models/agendamento.py
@@ -1,10 +1,23 @@
 from django.db import models
 
+from apps.agenda.enums import Dia, Status
+from config import settings
+
 
 class Agendamento(models.Model):
-    nome = models.CharField(max_length=255)
-    duracao = models.IntegerField()  # em minutos
-    valor = models.DecimalField()
-    ativo = models.BooleanField(default=True)  # type: ignore
+    inicio = models.DateTimeField()
+    fim = models.DateTimeField()
 
-    clinica = models.ForeignKey("Clinica", on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=15, choices=Status.choices, default=Status.PENDENTE
+    )
+
+    observacoes = models.TextField(null=True)
+
+    procedimento = models.ForeignKey("Procedimento", on_delete=models.PROTECT)
+    clinico = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("clinico", "dia", "ativo")
--- apps/agenda/models/expediente.py
+++ apps/agenda/models/expediente.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from apps.agenda.enums import Dia, Status
+from apps.agenda.enums import Dia
 from config import settings
 
 
@@ -9,15 +9,4 @@ class Expediente(models.Model):
     inicio = models.TimeField()
     fim = models.TimeField()
     ativo = models.BooleanField(default=True)  # type: ignore
-
-    status = models.CharField(
-        max_length=15, choices=Status.choices, default=Status.PENDENTE
-    )
-
-    procedimento = models.ForeignKey("Procedimento", on_delete=models.PROTECT)
     clinico = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-
-    criado_em = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ("clinico", "dia", "ativo")
